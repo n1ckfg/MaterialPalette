1b16a434e6b5925a37cfd2e1bc3a668e66b9648e
Nick Fox-Gieg <nick@fox-gieg.com>
2017-06-12 12:20:41 -0400

    testing

--- Assets/Scripts/MaterialPalette.cs
+++ Assets/Scripts/MaterialPalette.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MaterialPalette : MonoBehaviour {
+// Draws 3 meshes with the same material but with different colors.
+public class MaterialPalette : MonoBehaviour
+{
+	public Color[] color;
+	public Renderer[] target;
+
+	private MaterialPropertyBlock block;
 
 	void Start() {
-		
-	}
-	
-	void Update() {
-		
+		block = new MaterialPropertyBlock();
+
+		for (int i=0; i<target.Length; i++) {
+			target[i].GetPropertyBlock(block);
+			block.SetColor("_Color", color[i]);
+			target[i].SetPropertyBlock(block);
+		}
 	}
 
 }
